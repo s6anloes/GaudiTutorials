68938ddc5d35b0098d86e51fc864c2f36803a2be
Andreas Loeschcke Centeno <andreas.loeschcke.centeno@cern.ch>
2025-09-03 15:57:31 +0200

    Updated MoliereRadiusSolution to take EventStats output

--- MoliereRadiusFunctional/options/runMoliereRadius.py
+++ MoliereRadiusFunctional/options/runMoliereRadius.py
@@ -1,23 +1,31 @@
-import os
-
 from Gaudi.Configuration import INFO
 from k4FWCore import IOSvc, ApplicationMgr
-from Configurables import EventDataSvc, GeoSvc
+from Configurables import EventDataSvc
 
 io_svc = IOSvc("IOSvc")
-io_svc.Input = "../data/simplecalo1_example.root"
-io_svc.Output = "../data/output.root"
+# io_svc.Input = "/afs/cern.ch/work/a/adevita/public/DD4hepTutorials/simplecalo.root"
+io_svc.Input = "../../data/simplecalo1_example.root"
+io_svc.Output = "output.root"
+
+from Configurables import EventStats
+
+eventStats_functional = EventStats("EventStats",
+        
+    InputCaloHitCollection=["simplecaloRO"],
+    OutputEnergyBarycentre=["EnergyBarycentreX", "EnergyBarycentreY", "EnergyBarycentreZ"],
+    OutputTotalEnergy=["TotalEnergy"],
+    SaveHistograms=True,
+    OutputLevel=INFO
+)
 
 from Configurables import MoliereRadiusSolution
-moliere_radius_solution = MoliereRadiusSolution("MoliereRadiusSolution",
-                                                InputCaloHitCollection=["simplecaloRO"])
-
-from Configurables import MoliereRadius
-moliere_radius = MoliereRadius("MoliereRadius",
-                                InputCaloHitCollection=["simplecaloRO"])
+moliere_radius = MoliereRadiusSolution  ("MoliereRadius",
+                                InputCaloHitCollection=["simplecaloRO"],
+                                InputBarycenter=eventStats_functional.OutputEnergyBarycentre,
+                                InputTotalEnergy=eventStats_functional.OutputTotalEnergy)
 
 app_mgr = ApplicationMgr(
-    TopAlg=[moliere_radius_solution, moliere_radius],
+    TopAlg=[eventStats_functional, moliere_radius],
     EvtSel='NONE',
     EvtMax=-1,
     ExtSvc=[EventDataSvc("EventDataSvc")],
